739fbf19abf2117cb2df7114b1f89dd9172fa49a
Stanislav Poussep <stanislav.poussep@booking.com>
2018-11-23 14:23:35 +0100

    starting looking good

--- depac.pl
+++ depac.pl
@@ -5,10 +5,18 @@ use warnings qw(all);
 
 use AnyEvent::HTTP;
 use AnyEvent::Socket;
+use Getopt::Long;
 use IO::Socket;
 use JE;
 use Net::Domain;
 use URI;
+
+my $hostdomain = Net::Domain::hostdomain;
+my $exports = 1;
+GetOptions(
+    'hostdomain=s'      => \$hostdomain,
+    'exports!'          => \$exports,
+);
 
 my %memoize;
 my $je = JE->new;
@@ -72,7 +80,8 @@ $je->new_function(shExpMatch => sub {
     };
 });
 
-my @hostdomain = split m{\.}x, Net::Domain::hostdomain;
+AE::log info => 'searching domain %s', $hostdomain;
+my @hostdomain = split m{\.}x, $hostdomain;
 my $cv = AnyEvent->condvar;
 my $w = AnyEvent->timer(after => 1.0, cb => sub { $cv->send });
 while ($#hostdomain) {
@@ -94,110 +103,114 @@ my $body = $cv->recv;
 AE::log fatal => "COULDN'T FIND WPAD" unless $body;
 $je->eval($body);
 
-AE::log info => 'STARTING THE SERVER';
 my %pool;
-my $srv = tcp_server(
-    '127.0.0.1' => 8888,
-    sub {
-        my ($fh, $host, $port) = @_;
-        AE::log info => 'new connection from %s:%d (%d in pool)', $host, $port, scalar keys %pool;
-        my $h = AnyEvent::Handle->new(
-            fh          => $fh,
-            on_eof    => sub {
-                $_[0]->destroy;
-                delete $pool{ fileno($fh) };
-            },
-            on_error    => sub {
-                $_[0]->destroy;
-                delete $pool{ fileno($fh) };
-            },
-        );
-        $pool{ fileno($fh) } = $h;
-        $h->push_read(line => sub {
-            my ($_h, $line, $eol) = @_;
-            AE::log debug => '[from %s:%d] %s', $host, $port, $line;
-            my $url;
-            my ($verb, $peer_host, $peer_port, $proto);
-            if ($line =~ m{^(DELETE|GET|HEAD|OPTION|POST|PUT)\s+(https?://.+)\s+(HTTP/1\.[01])$}ix) {
-                $url = URI->new($2);
-                ($verb, $peer_host, $peer_port, $proto) = (uc($1), $url->host, $url->port, $3);
-            } elsif ($line =~ m{^CONNECT\s+([\w\.\-]+):([0-9]+)\s+(HTTP/1\.[01])$}ix) {
-                ($verb, $peer_host, $peer_port, $proto) = ('CONNECT', $1, $2, $3);
-            } else {
-                AE::log error => 'bad request from %s:%d', $host, $port;
-                $_h->push_write(
-                    'HTTP/1.0 400 Bad request' . $eol .
-                    'Cache-Control: no-cache' . $eol .
-                    'Connection: close' . $eol .
-                    'Content-Type: text/html' . $eol . $eol .
-                    '<html><body><h1>400 Bad request</h1></body></html>'
-                );
-                $_h->push_shutdown;
-                return;
-            }
-
-            # my $proxy = 'DIRECT';
-            my $proxy = $je->{FindProxyForURL}->(
-                ($verb eq 'CONNECT' ? 'https' : 'http') . '://' . $peer_host, # HACK!
-                $peer_host,
+tcp_server '127.0.0.1', undef, sub {
+    my ($fh, $host, $port) = @_;
+    AE::log info => 'new connection from %s:%d (%d in pool)', $host, $port, scalar keys %pool;
+    my $h = AnyEvent::Handle->new(
+        fh          => $fh,
+        on_eof    => sub {
+            $_[0]->destroy;
+            delete $pool{ fileno($fh) };
+        },
+        on_error    => sub {
+            $_[0]->destroy;
+            delete $pool{ fileno($fh) };
+        },
+    );
+    $pool{ fileno($fh) } = $h;
+    $h->push_read(line => sub {
+        my ($_h, $line, $eol) = @_;
+        AE::log debug => '[from %s:%d] %s', $host, $port, $line;
+        my $url;
+        my ($verb, $peer_host, $peer_port, $proto);
+        if ($line =~ m{^(DELETE|GET|HEAD|OPTIONS|POST|PUT|TRACE)\s+(https?://.+)\s+(HTTP/1\.[01])$}ix) {
+            $url = URI->new($2);
+            ($verb, $peer_host, $peer_port, $proto) = (uc($1), $url->host, $url->port, $3);
+        } elsif ($line =~ m{^CONNECT\s+([\w\.\-]+):([0-9]+)\s+(HTTP/1\.[01])$}ix) {
+            ($verb, $peer_host, $peer_port, $proto) = ('CONNECT', $1, $2, $3);
+        } else {
+            AE::log error => 'bad request from %s:%d', $host, $port;
+            $_h->push_write(
+                'HTTP/1.0 400 Bad request' . $eol .
+                'Cache-Control: no-cache' . $eol .
+                'Connection: close' . $eol .
+                'Content-Type: text/html' . $eol . $eol .
+                '<html><body><h1>400 Bad request</h1></body></html>'
             );
-            AE::log debug => 'WPAD says we should use %s', $proxy;
+            $_h->push_shutdown;
+            return;
+        }
 
-            my $peer_h;
-            if (uc($proxy) eq 'DIRECT') {
-                AE::log debug => 'connecting directly to %s:%d', $peer_host, $peer_port;
-                $peer_h = AnyEvent::Handle->new(
-                    connect     => [$peer_host => $peer_port],
-                    on_eof      => sub { $_[0]->destroy },
-                    on_error    => sub { $_[0]->destroy },
-                    on_connect  => sub {
-                        if ($verb eq 'CONNECT') {
-                            AE::log debug => '[to %s:%d]', $peer_host, $peer_port;
-                            $_h->push_read(regex => qr{(?:\015?\012){2}}x, sub {
-                                AE::log debug => 'skipping headers on CONNECT';
-                                $_h->push_write('HTTP/1.0 200 Connection established' . $eol . $eol);
-                            });
-                        } else {
-                            $line = join ' ', $verb, $url->path_query, $proto;
-                            AE::log debug => '[to %s:%d] %s', $peer_host, $peer_port, $line;
-                            $peer_h->push_write($line . $eol);
-                        }
-                        $_h->on_read(sub {
-                            AE::log trace => 'send %d bytes to %s:%d', length($_[0]->rbuf), $peer_host, $peer_port;
-                            $peer_h->push_write($_[0]->rbuf);
-                            $_[0]->rbuf = '';
+        # my $proxy = 'DIRECT';
+        my $proxy = $je->{FindProxyForURL}->(
+            ($verb eq 'CONNECT' ? 'https' : 'http') . '://' . $peer_host, # HACK!
+            $peer_host,
+        );
+        AE::log debug => 'WPAD says we should use %s', $proxy;
+
+        my $peer_h;
+        if ($proxy =~ m{^DIRECT\b}ix) {
+            AE::log debug => 'connecting directly to %s:%d', $peer_host, $peer_port;
+            $peer_h = AnyEvent::Handle->new(
+                connect     => [$peer_host => $peer_port],
+                on_eof      => sub { $_[0]->destroy },
+                on_error    => sub { $_[0]->destroy },
+                on_connect  => sub {
+                    if ($verb eq 'CONNECT') {
+                        AE::log debug => '[to %s:%d]', $peer_host, $peer_port;
+                        $_h->push_read(regex => qr{(?:\015?\012){2}}x, sub {
+                            AE::log debug => 'skipping headers on CONNECT';
+                            $_h->push_write('HTTP/1.0 200 Connection established' . $eol . $eol);
                         });
-                        $peer_h->on_read(sub {
-                            AE::log trace => 'recv %d bytes from %s:%d', length($_[0]->rbuf), $peer_host, $peer_port;
-                            $_h->push_write($_[0]->rbuf);
-                            $_[0]->rbuf = '';
-                        });
-                    }
-                );
-            } else {
-                my ($proxy_host, $proxy_port) = ($proxy =~ m{^PROXY\s+([\w\.]+):([0-9]+)}ix);
-                AE::log debug => 'connecting to %s:%d via %s:%d', $peer_host, $peer_port, $proxy_host, $proxy_port;
-                $peer_h = AnyEvent::Handle->new(
-                    connect     => [$proxy_host => $proxy_port],
-                    on_eof      => sub { $_[0]->destroy },
-                    on_error    => sub { $_[0]->destroy },
-                    on_connect  => sub {
-                        AE::log debug => '[to %s:%d] %s', $proxy_host, $proxy_port, $line;
+                    } else {
+                        $line = join ' ', $verb, $url->path_query, $proto;
+                        AE::log debug => '[to %s:%d] %s', $peer_host, $peer_port, $line;
                         $peer_h->push_write($line . $eol);
-                        $_h->on_read(sub {
-                            AE::log trace => 'send %d bytes to %s:%d', length($_[0]->rbuf), $proxy_host, $proxy_port;
-                            $peer_h->push_write($_[0]->rbuf);
-                            $_[0]->rbuf = '';
-                        });
-                        $peer_h->on_read(sub {
-                            AE::log trace => 'recv %d bytes from %s:%d', length($_[0]->rbuf), $proxy_host, $proxy_port;
-                            $_h->push_write($_[0]->rbuf);
-                            $_[0]->rbuf = '';
-                        });
                     }
-                );
-            }
-        });
-    }
-);
+                    $_h->on_read(sub {
+                        AE::log trace => 'send %d bytes to %s:%d', length($_[0]->rbuf), $peer_host, $peer_port;
+                        $peer_h->push_write($_[0]->rbuf);
+                        $_[0]->rbuf = '';
+                    });
+                    $peer_h->on_read(sub {
+                        AE::log trace => 'recv %d bytes from %s:%d', length($_[0]->rbuf), $peer_host, $peer_port;
+                        $_h->push_write($_[0]->rbuf);
+                        $_[0]->rbuf = '';
+                    });
+                }
+            );
+        } else {
+            my ($proxy_host, $proxy_port) = ($proxy =~ m{^PROXY\s+([\w\.]+):([0-9]+)}ix);
+            AE::log debug => 'connecting to %s:%d via %s:%d', $peer_host, $peer_port, $proxy_host, $proxy_port;
+            $peer_h = AnyEvent::Handle->new(
+                connect     => [$proxy_host => $proxy_port],
+                on_eof      => sub { $_[0]->destroy },
+                on_error    => sub { $_[0]->destroy },
+                on_connect  => sub {
+                    AE::log debug => '[to %s:%d] %s', $proxy_host, $proxy_port, $line;
+                    $peer_h->push_write($line . $eol);
+                    $_h->on_read(sub {
+                        AE::log trace => 'send %d bytes to %s:%d', length($_[0]->rbuf), $proxy_host, $proxy_port;
+                        $peer_h->push_write($_[0]->rbuf);
+                        $_[0]->rbuf = '';
+                    });
+                    $peer_h->on_read(sub {
+                        AE::log trace => 'recv %d bytes from %s:%d', length($_[0]->rbuf), $proxy_host, $proxy_port;
+                        $_h->push_write($_[0]->rbuf);
+                        $_[0]->rbuf = '';
+                    });
+                }
+            );
+        }
+    });
+}, sub {
+    my ($fh, $this_host, $this_port) = @_;
+    AE::log info => 'STARTING THE SERVER AT %s:%d', $this_host, $this_port;
+    return unless $exports;
+    my $proxy = URI->new('http://' . $this_host);
+    $proxy->port($this_port);
+    say qq(export $_="$proxy") for map { $_, uc } qw(http_proxy https_proxy);
+    say qq(export $_="localhost,127.0.0.1") for map { $_, uc } qw(no_proxy);
+};
 AnyEvent->condvar->wait;
